ff685aa0ea6024cedd6fcdb396c260ab5a99002b
新娘的味道 <1271005938@qq.com>
2017-07-26 11:49:48 +0800

    2

---
+++ src/ios/LZPlugin.m
@@ -0,0 +1,28 @@
+/********* LZPlugin.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+//列祖列宗来了这里这里这里列祖列宗来了
+@interface LZPlugin : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation LZPlugin
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
